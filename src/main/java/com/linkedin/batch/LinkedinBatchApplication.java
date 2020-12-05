0a81ede06ef20b368e589a19b7ac434e765a3bdf
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-12-05 18:25:54 +0200

    Initial commit

---
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -0,0 +1,49 @@
+package com.linkedin.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+// Causes several beans to be registered within Spring's IOC container: 
+// JobRepository, JobLauncher, JobRegistry & a transaction manager
+@EnableBatchProcessing
+public class LinkedinBatchApplication {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinBatchApplication.class, args);
+	}
+
+	@Bean
+	public Job deliverPackageJob() {
+		return this.jobBuilderFactory.get("deliverPackageJob").start(packageItemStep()).build();
+	}
+
+	@Bean
+	public Step packageItemStep() {
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("The item has been packaged");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+}
