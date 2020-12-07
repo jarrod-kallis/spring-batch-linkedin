db3000de5aa094e45e43230007f963ff16ae75e5
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-12-07 12:12:22 +0200

    Flat file chunk job

--- src/main/java/com/linkedin/batch/job/chunk/ChunkBasedJob.java
+++ src/main/java/com/linkedin/batch/job/chunk/ChunkBasedJob.java
@@ -8,12 +8,18 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class ChunkBasedJob {
+	
+	public static String[] headerNames = new String[] {"order_id", "first_name", "last_name", "email", "cost", "item_id", "item_name", "ship_date"};
 	
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
@@ -29,19 +35,34 @@ public class ChunkBasedJob {
 	}
 	
 	@Bean
-	public ItemReader<String> itemReader() {
-		return new SimpleItemReader();
+	public ItemReader<Order> itemReader() {
+		FlatFileItemReader<Order> itemReader = new FlatFileItemReader<Order>();
+		
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(new FileSystemResource("src/main/java/com/linkedin/batch/job/chunk/shipped_orders.csv"));
+		
+		DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<Order>();
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setNames(headerNames);
+		
+		lineMapper.setLineTokenizer(tokenizer);
+		
+		lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
+
+		itemReader.setLineMapper(lineMapper);
+		
+		return itemReader;
 	}	
 	
 	@Bean
 	public Step chunkStep() {		
 		return this.stepBuilderFactory.get("chunkStep")
-				.<String, String>chunk(3)
+				.<Order, Order>chunk(3)
 				.reader(itemReader())
-				.writer(new ItemWriter<String>() {
+				.writer(new ItemWriter<Order>() {
 
 					@Override
-					public void write(List<? extends String> items) throws Exception {
+					public void write(List<? extends Order> items) throws Exception {
 						System.out.println(String.format("Received %s items", items.size()));
 						items.forEach(System.out::println);
 					}
